4a46f1460444e30ef8a47a4abe4496ce167a1ee3
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2025-02-17 14:21:19 -0300

    Correção nome do site

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ HTML_TEMPLATE = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ChatBot Dark</title>
+    <title>ChatBot de Vendas</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
